66a89858ed336b62908ecd470db16a898f1144e8
Baxa-1998 <baxtiyornarziev6@gmail.com>
2024-11-07 00:03:40 +0300

    new commit

--- components/shared/Doctors/index.jsx
+++ components/shared/Doctors/index.jsx
@@ -125,7 +125,7 @@ export default function Doctors() {
     <section id='doctors' className="doctors">
       <div className="doctors__main_text">
         <div className="doctors__line"></div>
-        <h3 className="doctors__title">Наши докторы</h3>
+        <h3 className="doctors__title">Наши доктора</h3>
       </div>
       <Slider {...settings} className="slider-container">
        {doctors.map((doctor)=>(
